0f30f341ad509dad6b95c010995f1603760bfa5a
Jan Merhar <merharjan@gmail.com>
2020-06-25 23:39:20 +0200

    dodal kreiranje uprabnikove podmape

--- chooseDir.py
+++ chooseDir.py
@@ -1,10 +1,21 @@
 # https://www.youtube.com/watch?v=H71ts4XxWYU
 import tkinter as tk
 from tkinter import filedialog
+import os
 
-root = tk.Tk()
-root.withdraw()
+def izberiDir():
+    root = tk.Tk()
+    root.withdraw()
 
+    dir = filedialog.askdirectory()
+    return dir 
 
-dir = filedialog.askdirectory()
-print(dir)
+def narediDir(startDir):
+    end = input("Vnesi ime mape: ")
+    endDir = startDir + '/' + end 
+    os.mkdir(endDir)
+
+dir = izberiDir()
+narediDir(dir)
+
+#naredi kreiranje posameznih map in določanja .pripon
